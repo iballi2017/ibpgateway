b83c380fd737d491685dec9addc2f782f182380c
iballi2017 <iballi2017@gmail.com>
2020-08-26 20:50:04 +0100

    first commit into restructure branch

---
+++ testing/app.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function () {
+    // alert("jlgjglgu")
+
+
+    (function () {
+        if (!$(".form_control").val()) {
+            if (!$(".form_control").siblings().hasClass("sit")) {
+                $(".form_control").siblings().addClass("sit")
+                // if ($(".form_control").attr('placeholder')) {
+                //     $(".form_control").removeAttr('placeholder');
+                // }
+            }
+        }
+        if ($(".form_control").val()) {
+            if ($(".form_control").siblings().hasClass("sit")) {
+                $(".form_control").siblings().removeClass("sit")
+            }
+        }
+        // console.log("bnbb")
+        // console.log("hello")
+    }
+    )(jQuery);
+
+
+    $(".form_control").focus(function () {
+        // console.log($(this).siblings())
+        if ($(this).siblings().hasClass("sit")) {
+            if ($(this).siblings("label").hasClass("valid-till")) {
+                $(this).attr('placeholder', "00/00");
+            }
+            // console.log($(this).siblings("label"))
+            $(this).siblings().removeClass("sit")
+        }
+
+        console.log($(this).val())
+    })
+    $(".form_control").focusout(function () {
+        // console.log($(this).siblings())
+
+        if (!$(this).val()) {
+            if (!$(this).siblings().hasClass("sit")) {
+                $(this).siblings().addClass("sit")
+                if ($(this).attr('placeholder')) {
+                    $(this).removeAttr('placeholder');
+                }
+            }
+        }
+    })
+})
